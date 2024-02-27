c8462fbf3a183d9adac2989f1d60c2b052b262ed
ovseychiks <ovseychiks@kacd.kz>
2024-02-27 20:55:59 +0600

    Bean scope and lifecycle

--- src/main/java/kz/learn/springcourse/TestSpring.java
+++ src/main/java/kz/learn/springcourse/TestSpring.java
@@ -6,11 +6,24 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 
-        musicPlayer.playMusic();
+        boolean comparison = firstMusicPlayer == secondMusicPlayer;
+        System.out.println(comparison);
+
+        System.out.println(firstMusicPlayer);
+        System.out.println(secondMusicPlayer);
+
+        firstMusicPlayer.playMusic();
+        firstMusicPlayer.setVolume(10);
+
+        System.out.println(firstMusicPlayer.getVolume());
+        System.out.println(secondMusicPlayer.getVolume());
+/*
         System.out.println(musicPlayer.getName());
         System.out.println(musicPlayer.getVolume());
+*/
 
         context.close();
     }
