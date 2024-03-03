6ac260b1f0b5448a78b2813bd0079a11e137d7d8
ovseychiks <ovseychiks@kacd.kz>
2024-03-03 15:54:55 +0500

    Annotations. PostConstruct and PreDestroy

--- src/main/java/kz/learn/springcourse/ClassicalMusic.java
+++ src/main/java/kz/learn/springcourse/ClassicalMusic.java
@@ -1,11 +1,13 @@
 package kz.learn.springcourse;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
 @Component
+@Scope("singleton")
 public class ClassicalMusic implements Music {
     @PostConstruct
     public void doMyInit() {
