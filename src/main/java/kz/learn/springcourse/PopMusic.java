8e59b8b40cc5b8c57bafebc48bc1bfe7783407a6
ovseychiks <ovseychiks@kacd.kz>
2024-03-03 17:55:43 +0500

    Adds bean creation to SpringConfig.java

--- src/main/java/kz/learn/springcourse/PopMusic.java
+++ src/main/java/kz/learn/springcourse/PopMusic.java
@@ -1,10 +1,5 @@
 package kz.learn.springcourse;
 
-import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Component;
-
-@Component
-@Scope("prototype")
 public class PopMusic implements Music {
     @Override
     public String getSong() {
--- src/main/java/kz/learn/springcourse/SpringConfig.java
+++ src/main/java/kz/learn/springcourse/SpringConfig.java
@@ -1,5 +1,6 @@
 package kz.learn.springcourse;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -8,4 +9,23 @@ import org.springframework.context.annotation.PropertySource;
 @ComponentScan("kz.learn.springcourse")
 @PropertySource("classpath:musicPlayer.properties")
 public class SpringConfig {
+    @Bean
+    public ClassicalMusic classicalMusic() {
+        return new ClassicalMusic();
+    }
+
+    @Bean
+    public PopMusic popMusic() {
+        return new PopMusic();
+    }
+
+    @Bean
+    public MusicPlayer musicPlayer() {
+        return new MusicPlayer(popMusic(), classicalMusic());
+    }
+
+    @Bean
+    public Computer computer() {
+        return new Computer(musicPlayer());
+    }
 }
